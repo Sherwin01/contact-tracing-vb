8170520ff927804fab64ed7d1c1ee80cdef12a16
lim <sherwinlim8899@gmail.com>
2022-06-20 11:06:31 +0800

    3rd

--- contact-tracing-vb/contact-tracing-vb/Form1.Designer.vb
+++ contact-tracing-vb/contact-tracing-vb/Form1.Designer.vb
@@ -22,62 +22,64 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.addbtn = New System.Windows.Forms.Button()
+        Me.lb1 = New System.Windows.Forms.ListBox()
+        Me.FullName = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.savebtn = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.clearbtn = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.tb1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'Button1
+        'addbtn
         '
-        Me.Button1.Location = New System.Drawing.Point(601, 75)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(81, 24)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Add"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.addbtn.Location = New System.Drawing.Point(601, 75)
+        Me.addbtn.Name = "addbtn"
+        Me.addbtn.Size = New System.Drawing.Size(81, 24)
+        Me.addbtn.TabIndex = 0
+        Me.addbtn.Text = "Add"
+        Me.addbtn.UseVisualStyleBackColor = True
         '
-        'ListBox1
+        'lb1
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 15
-        Me.ListBox1.Location = New System.Drawing.Point(246, 135)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(316, 154)
-        Me.ListBox1.TabIndex = 1
+        Me.lb1.FormattingEnabled = True
+        Me.lb1.ItemHeight = 15
+        Me.lb1.Location = New System.Drawing.Point(246, 135)
+        Me.lb1.Name = "lb1"
+        Me.lb1.Size = New System.Drawing.Size(316, 154)
+        Me.lb1.TabIndex = 1
         '
-        'TextBox1
+        'FullName
         '
-        Me.TextBox1.Location = New System.Drawing.Point(246, 75)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(316, 23)
-        Me.TextBox1.TabIndex = 2
+        Me.FullName.Location = New System.Drawing.Point(246, 32)
+        Me.FullName.Name = "FullName"
+        Me.FullName.Size = New System.Drawing.Size(316, 23)
+        Me.FullName.TabIndex = 2
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(179, 135)
+        Me.Label1.Location = New System.Drawing.Point(177, 35)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(61, 15)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Full Name"
         '
-        'Button2
+        'savebtn
         '
-        Me.Button2.Location = New System.Drawing.Point(601, 221)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(81, 23)
-        Me.Button2.TabIndex = 4
-        Me.Button2.Text = "Save"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.savebtn.Location = New System.Drawing.Point(601, 221)
+        Me.savebtn.Name = "savebtn"
+        Me.savebtn.Size = New System.Drawing.Size(81, 23)
+        Me.savebtn.TabIndex = 4
+        Me.savebtn.Text = "Save"
+        Me.savebtn.UseVisualStyleBackColor = True
         '
         'Label2
         '
@@ -91,7 +93,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(212, 150)
+        Me.Label3.Location = New System.Drawing.Point(210, 135)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(28, 15)
         Me.Label3.TabIndex = 6
@@ -100,7 +102,7 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(195, 165)
+        Me.Label4.Location = New System.Drawing.Point(189, 150)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(51, 15)
         Me.Label4.TabIndex = 7
@@ -109,7 +111,7 @@ Partial Class Form1
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(199, 180)
+        Me.Label5.Location = New System.Drawing.Point(193, 165)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(45, 15)
         Me.Label5.TabIndex = 8
@@ -118,7 +120,7 @@ Partial Class Form1
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(195, 196)
+        Me.Label6.Location = New System.Drawing.Point(189, 180)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(49, 15)
         Me.Label6.TabIndex = 9
@@ -127,38 +129,56 @@ Partial Class Form1
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(150, 211)
+        Me.Label7.Location = New System.Drawing.Point(144, 195)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(96, 15)
         Me.Label7.TabIndex = 10
         Me.Label7.Text = "Contact Number"
         '
-        'Button3
+        'clearbtn
         '
-        Me.Button3.Location = New System.Drawing.Point(601, 250)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(81, 23)
-        Me.Button3.TabIndex = 11
-        Me.Button3.Text = "Clear"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.clearbtn.Location = New System.Drawing.Point(601, 250)
+        Me.clearbtn.Name = "clearbtn"
+        Me.clearbtn.Size = New System.Drawing.Size(81, 23)
+        Me.clearbtn.TabIndex = 11
+        Me.clearbtn.Text = "Clear"
+        Me.clearbtn.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(179, 80)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(59, 15)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "Type Here"
+        '
+        'tb1
+        '
+        Me.tb1.Location = New System.Drawing.Point(246, 76)
+        Me.tb1.Name = "tb1"
+        Me.tb1.Size = New System.Drawing.Size(316, 23)
+        Me.tb1.TabIndex = 13
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.tb1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.clearbtn)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.savebtn)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.ListBox1)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.FullName)
+        Me.Controls.Add(Me.lb1)
+        Me.Controls.Add(Me.addbtn)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -166,16 +186,18 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents ListBox1 As ListBox
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents addbtn As Button
+    Friend WithEvents lb1 As ListBox
+    Friend WithEvents FullName As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents Button2 As Button
+    Friend WithEvents savebtn As Button
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
-    Friend WithEvents Button3 As Button
+    Friend WithEvents clearbtn As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents tb1 As TextBox
 End Class
--- contact-tracing-vb/contact-tracing-vb/Form1.vb
+++ contact-tracing-vb/contact-tracing-vb/Form1.vb
@@ -1,7 +1,27 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        ListBox1.Items.Add(TextBox1.Text)
+    Private Sub addbtn_Click(sender As Object, e As EventArgs) Handles addbtn.Click
+        lb1.Items.Add(tb1.Text)
+
+
     End Sub
 
+    Private Sub savebtn_Click(sender As Object, e As EventArgs) Handles savebtn.Click
 
+        Dim file As System.IO.StreamWriter
+        Dim x As Integer
+        file = New IO.StreamWriter("C:\Users\Lim\OneDrive\Desktop\vbassignment\" & FullName.Text & ".txt")
+        For x = 0 To lb1.Items.Count - 1
+            file.WriteLine(lb1.Items.Item(x))
+        Next
+
+        file.Close()
+
+    End Sub
+
+    Private Sub clearbtn_Click(sender As Object, e As EventArgs) Handles clearbtn.Click
+
+
+        lb1.Items.Clear()
+
+    End Sub
 End Class
